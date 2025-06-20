c6e58ec21935cc41efa7e8874dc5f7b5f1f7c5ab
JeffDRK <jastudillo_1510@hotmail.com>
2025-06-20 21:58:14 +0000

    update

--- MiCasitaCIDetection/train.py
+++ MiCasitaCIDetection/train.py
@@ -16,16 +16,16 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.train(
         data="/root/jastudillo/Trabajo/Training_Cedulas2.0 (2)/data.yaml",  # Ruta al archivo de configuración
-        epochs=500,  # Número de épocas
+        epochs=1000,  # Número de épocas
         imgsz=640,  # Tamaño de entrada de imagen
-        batch=64,  # Tamaño del batch
+        batch=512,  # Tamaño del batch
         workers=4,  # Núm. de workers para carga de datos
-        device=5,  # Usa la GPU 0; usa "cpu" para CPU
+        device=[-1,-1],  # Usa la GPU 0; usa "cpu" para CPU
         name="yolo11_obb_custom",  # Nombre de la carpeta del experimento
         verbose=True,
         patience=100,
-        weight_decay=1e-6,
-        lr0=1e-4,
+        weight_decay=1e-8,
+        lr0=1e-5,
         lrf=0.01,
         degrees=180,
         optimizer="AdamW",
