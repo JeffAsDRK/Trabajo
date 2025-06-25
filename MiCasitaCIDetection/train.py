dc736e052d9f63e9b5f3dd3f7808de001cbbfc02
JeffDRK <jastudillo_1510@hotmail.com>
2025-06-25 17:03:36 -0500

    update

--- MiCasitaCIDetection/train.py
+++ MiCasitaCIDetection/train.py
@@ -1,13 +1,9 @@
 import torch
 from ultralytics import YOLO
-
-
-
 # Cargar un modelo base (puedes cambiar a yolov8s.pt, yolov8m.pt, etc.)
 model = YOLO(
     "/root/jastudillo/Trabajo/MiCasitaCIDetection/best_v180.pt"
 )  # o "yolov8s.pt", o "yolov8n.yaml" para comenzar desde cero
-
 # Entrenar el modelo
 if __name__ == "__main__":
     print(torch.cuda.is_available())  # ¿True?
@@ -20,13 +16,20 @@ if __name__ == "__main__":
         imgsz=640,  # Tamaño de entrada de imagen
         batch=512,  # Tamaño del batch
         workers=4,  # Núm. de workers para carga de datos
-        device=[-1,-1],  # Usa la GPU 0; usa "cpu" para CPU
+        device=-1,  # Usa la GPU 0; usa "cpu" para CPU
         name="yolo11_obb_custom",  # Nombre de la carpeta del experimento
         verbose=True,
         patience=100,
         weight_decay=1e-8,
-        lr0=1e-5,
-        lrf=0.01,
-        degrees=180,
+        lr0=1e-4,
+        lrf=0.001,
+        degrees=90,
         optimizer="AdamW",
+        hsv_v=0.8,
+        translate=0.25,
+        scale=0.8,
+        shear=10,
+        perspective=0.001,
+        flipud=0.7,
+        copy_paste_mode="mixup",
     )
